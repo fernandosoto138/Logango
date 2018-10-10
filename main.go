6121c02e7309f3692a71d5fd1f7625a76e898a73
Fernando Soto <fernando.soto.94@hotmail.com>
2018-10-10 15:57:06 -0300

    added flags in a struct

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
-import "flag"
+import (
+	"flag"
+)
 
-func main() {
-	/* This is my first sample program. */
+type cliParams struct {
+	log string
+}
+
+func parseArguments() cliParams {
+	var params cliParams
 	logFile := flag.String("logfile", "", "Path to the log")
 	flag.Parse()
-	fmt.Printf("The log is : %s\n", *logFile)
+	params.log = *logFile
+	return params
+}
+
+func main() {
+	args := parseArguments()
+	println(args.log)
 }
