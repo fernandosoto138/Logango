88560fb19a54cc0b889888e024babacc0e4d413a
Fernando Soto <fernando.soto.94@hotmail.com>
2018-10-16 17:24:07 -0300

    implemented argumetns and config file

--- main.go
+++ main.go
@@ -2,37 +2,31 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"os"
 )
 
-type cliParams struct {
-	log string
-}
-
-func parseArguments() cliParams {
-	var params cliParams
-	logFile := flag.String("logfile", "", "Path to the log")
-	flag.Parse()
-	params.log = *logFile
-	return params
-}
-
-func checkFile(err error) {
+func checkFile(err error, file string) {
 	if err != nil {
+		fmt.Println("Problems loading the file " + file)
 		panic(err)
+
 	}
 }
 
 func main() {
 	args := parseArguments()
+	config := NewConfig(args.config)
+	if config == nil {
+		panic("Problems loading the config file " + args.config)
+	}
+	fmt.Printf("%v", config)
 	file, err := os.Open(args.log)
-	checkFile(err)
+	checkFile(err, args.log)
+	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
-		fmt.Printf(scanner.Text())
-		fmt.Printf("\n")
+		fmt.Println(scanner.Text())
 	}
 }
